f43fceee88af6cce1037dd10360f330bcb847ece
Diana Vladotsenko <diana.vladotsenko@gmail.com>
2025-02-10 22:54:55 +0200

    added kymnevoistlus

---
+++ 01_07.02_andmemudelid/kymnevoistlus/src/test/java/ee/dianaV/online/store/OnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.dianaV.online.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
